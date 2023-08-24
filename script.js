82c41b52809ef5326a265d4ce953f4d733096aad
Rangga Agastya <rangga.agastya711@gmail.com>
2023-08-24 23:23:20 +0700

    menampilkan deskripsi film

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const API_KEY = "8279cf5a";
 
-function getAllMovies(ajax) {
+function getMovies(ajax) {
   const xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function () {
@@ -17,29 +17,77 @@ function getAllMovies(ajax) {
   xhr.send();
 }
 
-getAllMovies({
-  url: `http://www.omdbapi.com/?apikey=${API_KEY}&s=avengers`,
+// Get Movies by Title
+getMovies({
+  url: `http://www.omdbapi.com/?apikey=${API_KEY}&s=naruto`,
   success: (result) => {
     const movies = result.Search;
     const movieList = document.querySelector("#movie-list");
+    let el = "";
 
     movies.forEach((movie) => {
-      let el = `<div class="card">
+      el += `<div class="col-lg-4 mb-3">
+        <div class="card">
             <img src="${movie.Poster}" class="card-img-top" />
             <div class="card-body">
               <h5 class="card-title mb-4">${movie.Title}</h5>
               <h6 class="card-subtitle mb-2 text-body-secondary">Tahun rilis: ${movie.Year}</h6>
               <h6 class="card-subtitle mb-4 text-body-secondary">Tipe: ${movie.Type}</h6>
-              <a href="#" class="btn btn-primary">Lihat Detail</a>
+              <button
+                type="button"
+                class="btn btn-primary lihat-detail"
+                data-bs-toggle="modal"
+                data-bs-target="#detail-film"
+                data-imdb="${movie.imdbID}"
+              >
+                Lihat Detail
+              </button>
             </div>
-          </div>`;
+          </div>
+      </div>`;
+    });
 
-      let html = document.createElement("div");
-      html.classList.add("col-lg-4");
-      html.classList.add("mb-3");
-      html.innerHTML = el;
+    movieList.innerHTML = el;
 
-      movieList.appendChild(html);
+    // Get Movie By imdbID
+    const btn_lihat_detail = document.querySelectorAll(".lihat-detail");
+
+    btn_lihat_detail.forEach((btn) => {
+      btn.addEventListener("click", function () {
+        const modalFilm = document.querySelector("#detail-film");
+
+        const judul = modalFilm.querySelector("#judul");
+        const plot = modalFilm.querySelector(".plot");
+        const genre = modalFilm.querySelector(".genre");
+        const tanggal_rilis = modalFilm.querySelector(".tanggal-rilis");
+        const durasi_film = modalFilm.querySelector(".durasi-film");
+        const sutradara = modalFilm.querySelector(".sutradara");
+        const penulis = modalFilm.querySelector(".penulis");
+        const pemeran = modalFilm.querySelector(".pemeran");
+        const ratings = modalFilm.querySelector(".ratings");
+
+        getMovies({
+          url: `https://www.omdbapi.com/?apikey=${API_KEY}&i=${this.dataset.imdb}`,
+          success: (movie) => {
+            judul.textContent = movie.Title;
+            plot.textContent = movie.Plot;
+            genre.textContent = movie.Genre;
+            tanggal_rilis.textContent = movie.Released;
+            durasi_film.textContent = movie.Runtime;
+            sutradara.textContent = movie.Director;
+            penulis.textContent = movie.Writer;
+            pemeran.textContent = movie.Actors;
+
+            let el = "";
+            movie.Ratings.forEach((rate) => {
+              el += `<li>${rate.Source}: ${rate.Value}</li>`;
+            });
+
+            ratings.innerHTML = el;
+          },
+          error: () => {},
+        });
+      });
     });
   },
   error: (e) => console.error(e),
